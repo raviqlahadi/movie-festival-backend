db302fd2868dcc2fcbedc8b4a1bc741e92e599a7
raviqlahadi <raviq.lahadi@laku6.com>
2024-11-20 22:48:18 +0700

    fix migration file

--- migrations/000004_make_user_id_nullable.down.sql
+++ migrations/000004_make_user_id_nullable.down.sql
@@ -1,3 +1,6 @@
 -- Alter the user_id column in the viewership table to allow NULL values
 ALTER TABLE viewership
 ALTER COLUMN user_id DROP NOT NULL;
+
+-- Drop the foreign key constraint on user_id
+ALTER TABLE viewership DROP CONSTRAINT IF EXISTS viewership_user_id_fkey;
--- migrations/000004_make_user_id_nullable.up.sql
+++ migrations/000004_make_user_id_nullable.up.sql
@@ -1,3 +1,10 @@
 -- Revert the user_id column in the viewership table to NOT NULL
 ALTER TABLE viewership
 ALTER COLUMN user_id SET NOT NULL;
+
+-- Re-add the foreign key constraint on user_id
+ALTER TABLE viewership
+ADD CONSTRAINT viewership_user_id_fkey
+FOREIGN KEY (user_id)
+REFERENCES users(id)
+ON DELETE CASCADE;
