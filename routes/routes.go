4825fb003b51a16f88ab3230e4784bfb85b5794d
raviqlahadi <raviq.lahadi@laku6.com>
2024-11-21 03:22:10 +0700

    implement middleware

--- routes/routes.go
+++ routes/routes.go
@@ -5,39 +5,52 @@ import (
 
 	"github.com/gorilla/mux"
 	"github.com/raviqlahadi/movie-festival-backend/internal/handlers"
+	"github.com/raviqlahadi/movie-festival-backend/internal/middleware"
 	"github.com/raviqlahadi/movie-festival-backend/internal/repositories"
 	"github.com/raviqlahadi/movie-festival-backend/internal/services"
 )
 
 func InitRoutes() *mux.Router {
 	router := mux.NewRouter()
-	secret_key := os.Getenv("SECRET_KEY")
+	secretKey := os.Getenv("SECRET_KEY")
 
-	//Load repository
+	// Load repositories
 	genreRepo := repositories.NewGenreRepository()
 	movieRepo := repositories.NewMovieRepository(genreRepo)
 	userRepo := repositories.NewUserRepository()
 	voteRepo := repositories.NewVoteRepository()
-	authService := services.NewAuthService(secret_key)
+	authService := services.NewAuthService(secretKey)
 
+	// Load handlers
 	AdminHandler := handlers.AdminHandler{MovieRepo: movieRepo}
 	MovieHandler := handlers.MovieHandler{MovieRepo: movieRepo}
 	AuthHandler := handlers.AuthHandler{UserRepo: userRepo, AuthService: authService}
 	VoteHandler := handlers.VoteHandler{VoteRepo: voteRepo}
 
-	//router path
-	router.HandleFunc("/admin/movies", AdminHandler.CreateMovie).Methods("POST")
-	router.HandleFunc("/admin/movies/{id}", AdminHandler.UpdateMovie).Methods("PUT")
-	router.HandleFunc("/admin/movies/most-viewed", AdminHandler.GetMostViewedMoviesAndGenreas).Methods("GET")
-	router.HandleFunc("/admin/movies/most-voted", VoteHandler.MostVotedMovie).Methods("GET")
+	// Middleware
+	authMiddleware := middleware.AuthMiddleware(secretKey)
+
+	// Public routes
 	router.HandleFunc("/auth/register", AuthHandler.Register).Methods("POST")
 	router.HandleFunc("/auth/login", AuthHandler.Login).Methods("POST")
 	router.HandleFunc("/movies", MovieHandler.ListMovies).Methods("GET")
 	router.HandleFunc("/movies/search", MovieHandler.SearchMovies).Methods("GET")
 	router.HandleFunc("/movies/{id}/view", MovieHandler.TrackViewership).Methods("POST")
-	router.HandleFunc("/movies/{movie_id}/vote", VoteHandler.VoteMovie).Methods("POST")
-	router.HandleFunc("/movies/{movie_id}/unvote", VoteHandler.UnvoteMovie).Methods("DELETE")
-	router.HandleFunc("/user/votes", VoteHandler.ListUserVotes).Methods("GET")
+
+	// Protected user routes
+	userRouter := router.PathPrefix("/").Subrouter()
+	userRouter.Use(authMiddleware) // Apply auth middleware to user routes
+	userRouter.HandleFunc("/movies/{movie_id}/vote", VoteHandler.VoteMovie).Methods("POST")
+	userRouter.HandleFunc("/movies/{movie_id}/unvote", VoteHandler.UnvoteMovie).Methods("DELETE")
+	userRouter.HandleFunc("/user/votes", VoteHandler.ListUserVotes).Methods("GET")
+
+	// Protected admin routes
+	adminRouter := router.PathPrefix("/admin").Subrouter()
+	adminRouter.Use(authMiddleware) // Apply auth middleware to admin routes
+	adminRouter.HandleFunc("/movies", AdminHandler.CreateMovie).Methods("POST")
+	adminRouter.HandleFunc("/movies/{id}", AdminHandler.UpdateMovie).Methods("PUT")
+	adminRouter.HandleFunc("/movies/most-viewed", AdminHandler.GetMostViewedMoviesAndGenreas).Methods("GET")
+	adminRouter.HandleFunc("/movies/most-voted", VoteHandler.MostVotedMovie).Methods("GET")
 
 	return router
 }
